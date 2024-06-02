034b034a6db4fe3a326bbb10a6c4323397181995
Lorenzo Stella <lorenzostella@gmail.com>
2024-06-02 21:21:05 +0200

    minor improvement

--- test/test_loscheme.py
+++ test/test_loscheme.py
@@ -3,64 +3,64 @@ import pytest
 from loscheme import Environment, parse, evaluate_expression
 
 
-@pytest.mark.parametrize(
-    "code, values",
-    [
-        (
-            """
-            (define a 3)
-            (define b 4)
-            (+ a b)
-            (define a 5)
-            a
-            (if (< a 6) 42 27)
-            (if (> a 13) 65 99)
-            (if (= a 5) 0 1)
-            """,
-            [None, None, 7, None, 5, 42, 99, 0],
-        ),
-        (
-            """
-            (define x 42)
-            (define f (lambda (x y) (+ (* 2 x) y)))
-            (f 4 5)
-            (define a 5)
-            (f (f 4 a) (+ 3 2))
-            x
-            """,
-            [None, None, 13, None, 31, 42],
-        ),
-        (
-            """
-            (define a 4)
-            (define x 100)
-            (define f (lambda (x) (+ 3 5) (+ a x)))
-            (f 3)
-            (define a 21)
-            (f 4)
-            """,
-            [None, None, None, 7, None, 25],
-        ),
-        (
-            """
-            (define a 3)
-            (begin (define a 5) (+ a 9))
-            a
-            """,
-            [None, 14, 5],
-        ),
-        (
-            """
-            (define a 3)
-            (let ((a 17) (b 4)) (+ a b) (* a b))
-            a
-            (let ((b 42)) (define a b) a)
-            a
-            """,
-            [None, 68, 3, 42, 3],
-        ),
-    ],
-)
+CODE_VALUES = [
+    (
+        """
+        (define a 3)
+        (define b 4)
+        (+ a b)
+        (define a 5)
+        a
+        (if (< a 6) 42 27)
+        (if (> a 13) 65 99)
+        (if (= a 5) 0 1)
+        """,
+        [None, None, 7, None, 5, 42, 99, 0],
+    ),
+    (
+        """
+        (define x 42)
+        (define f (lambda (x y) (+ (* 2 x) y)))
+        (f 4 5)
+        (define a 5)
+        (f (f 4 a) (+ 3 2))
+        x
+        """,
+        [None, None, 13, None, 31, 42],
+    ),
+    (
+        """
+        (define a 4)
+        (define x 100)
+        (define f (lambda (x) (+ 3 5) (+ a x)))
+        (f 3)
+        (define a 21)
+        (f 4)
+        """,
+        [None, None, None, 7, None, 25],
+    ),
+    (
+        """
+        (define a 3)
+        (begin (define a 5) (+ a 9))
+        a
+        """,
+        [None, 14, 5],
+    ),
+    (
+        """
+        (define a 3)
+        (let ((a 17) (b 4)) (+ a b) (* a b))
+        a
+        (let ((b 42)) (define a b) a)
+        a
+        """,
+        [None, 68, 3, 42, 3],
+    ),
+]
+
+
+@pytest.mark.parametrize("code, values", CODE_VALUES)
 def test_eval(code: str, values: list):
     env = Environment.standard_environment().create_child()
     expressions = parse(code)
