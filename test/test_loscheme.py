68f7dee271092c09ed7c66c428bc37cf51933503
Lorenzo Stella <lorenzostella@gmail.com>
2024-06-02 20:26:01 +0200

    simplify test setup

--- test/test_loscheme.py
+++ test/test_loscheme.py
@@ -1,5 +1,3 @@
-import textwrap
-
 import pytest
 
 from loscheme import Environment, parse, evaluate_expression
@@ -9,41 +7,35 @@ from loscheme import Environment, parse, evaluate_expression
     "code, values",
     [
         (
-            textwrap.dedent(
-                """
-                (define a 3)
-                (define b 4)
-                (+ a b)
-                (define a 5)
-                a
-                (if (< a 6) 42 27)
-                (if (> a 13) 65 99)
-                (if (= a 5) 0 1)
-                """
-            ),
+            """
+            (define a 3)
+            (define b 4)
+            (+ a b)
+            (define a 5)
+            a
+            (if (< a 6) 42 27)
+            (if (> a 13) 65 99)
+            (if (= a 5) 0 1)
+            """,
             [None, None, 7, None, 5, 42, 99, 0],
         ),
         (
-            textwrap.dedent(
-                """
-                (define x 42)
-                (define f (lambda (x y) (+ (* 2 x) y)))
-                (f 4 5)
-                (define a 5)
-                (f (f 4 a) (+ 3 2))
-                x
-                """
-            ),
+            """
+            (define x 42)
+            (define f (lambda (x y) (+ (* 2 x) y)))
+            (f 4 5)
+            (define a 5)
+            (f (f 4 a) (+ 3 2))
+            x
+            """,
             [None, None, 13, None, 31, 42],
         ),
         (
-            textwrap.dedent(
-                """
-                (define a 3)
-                (begin (define a 5) (+ a 9))
-                a
-                """
-            ),
+            """
+            (define a 3)
+            (begin (define a 5) (+ a 9))
+            a
+            """,
             [None, 14, 5],
         ),
     ],
