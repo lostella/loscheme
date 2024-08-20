d2e6e009ecbb813e25e7ca04a5cb3880b7578ada
Lorenzo Stella <lorenzostella@gmail.com>
2024-08-20 09:09:57 +0200

    fmt

--- rust/src/bin/repl.rs
+++ rust/src/bin/repl.rs
@@ -13,17 +13,18 @@ fn main() {
         let mut input = String::new();
 
         match stdin.lock().read_line(&mut input) {
-            Ok(0) => {  // Ctrl+D sends EOF (read_line returns 0 bytes read)
+            Ok(0) => {
+                // Ctrl+D sends EOF (read_line returns 0 bytes read)
                 println!("\nSkipped!");
                 continue; // Skip this iteration and prompt for input again
-            },
+            }
             Ok(_) => {
                 input = input.trim().to_string();
                 // Modify the input here (e.g., convert to uppercase)
                 let output = input.to_uppercase();
 
                 println!("Modified: {}", output);
-            },
+            }
             Err(err) => {
                 println!("Error: {}", err);
                 break;
