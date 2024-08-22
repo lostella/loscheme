f6d44f2a4cba403335471b1db1b4a192a8c3cb2c
Lorenzo Stella <lorenzostella@gmail.com>
2024-08-22 09:28:10 +0200

    fix repl

--- src/bin/repl.rs
+++ src/bin/repl.rs
@@ -2,9 +2,6 @@ use loscheme::{parse_code, Environment};
 use std::io::{self, BufRead, Write};
 
 fn main() {
-    println!("(Ctrl+D to skip, Ctrl+C to quit)");
-    println!();
-
     let stdin = io::stdin();
     let mut stdout = io::stdout();
     let mut env = Environment::standard().child();
