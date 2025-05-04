54acf2dc19d05d24e5a773ff1aa8366bd97b8a17
Lorenzo Stella <lorenzostella@gmail.com>
2025-05-04 21:03:12 +0200

    improve quicksort example

--- scripts/quicksort.scm
+++ scripts/quicksort.scm
@@ -8,4 +8,25 @@
           (list pivot)
           (quicksort (filter (lambda (x) (>= x pivot)) rest))))))
 
-(quicksort '(34 7 23 32 5 62 32 2 1 6 45 78 99 3))
+(define (build-random-list n)
+  (define (rand n) (remainder (+ (* n 22695477) 1) 4294967296))
+  (define (iter i seed acc)
+    (if (= i 0)
+        acc
+        (let ((new-seed (rand seed)))
+          (iter (- i 1) new-seed (cons (remainder new-seed 100000) acc)))))
+  (iter n 42 '()))
+
+(define (last lst)
+  (if (null? (cdr lst))
+      (car lst)
+      (last (cdr lst))))
+
+(define test-list (build-random-list 1000))
+(define sorted (quicksort test-list))
+(write (length sorted))
+(newline)
+(write (car sorted))
+(newline)
+(write (last sorted))
+(newline)
