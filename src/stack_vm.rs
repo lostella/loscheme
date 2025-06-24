661a7888bbf5cdac4710003a0b53c972c11c6aaf
Lorenzo Stella <lorenzostella@gmail.com>
2025-06-24 21:21:41 +0200

    fixes

--- src/stack_vm.rs
+++ src/stack_vm.rs
@@ -159,15 +159,15 @@ mod tests {
     fn test_fib() {
         let code = vec![
             // main:
-            Push { value: Int(6) }, // argument for fib
-            Call { addr: 3, nargs: 1 },   // call fib(20)
-            Halt,         // halt
+            Push { value: Int(6) },     // argument for fib
+            Call { addr: 3, nargs: 1 }, // call fib(20)
+            Halt,                       // halt
             // fib:
-            Push { value: Int(1) },    // push 1
-            LoadArg { narg: 0 },      // put n on the stack
+            Push { value: Int(1) },     // push 1
+            LoadArg { narg: 0 },        // put n on the stack
             JumpLessThan { offset: 3 }, // if 1 < n, jump to recursive case
-            LoadArg { narg: 0 },      // put n on the stack
-            Ret,             // return n
+            LoadArg { narg: 0 },        // put n on the stack
+            Ret,                        // return n
             LoadArg { narg: 0 },
             Push { value: Int(1) },
             Sub,
