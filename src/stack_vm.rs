a6cbb06a8a8cfbfaec594623e6a30de6f4404a44
Lorenzo Stella <lorenzostella@gmail.com>
2025-07-12 18:01:02 +0200

    small update to call conventions

--- src/stack_vm.rs
+++ src/stack_vm.rs
@@ -1,4 +1,5 @@
-use std::fmt;
+use crate::parser::{Expr, Keyword};
+use std::{collections::HashMap, fmt};
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Value {
@@ -17,7 +18,7 @@ pub enum Instruction {
     Jump { offset: i16 },
     JumpIfTrue { offset: i16 },
     Call { addr: usize, nargs: u8 },
-    LoadArg { narg: u8 },
+    LoadLocal { idx: u8 },
     Ret,
     Print,
     Halt,
@@ -53,6 +54,12 @@ impl VM {
             fp: 0,
             ip: 0,
         }
+    }
+
+    pub fn clone_stack(self) -> Vec<Value> {
+        // pop does not actually shrink the stack
+        // we clone only up to sp, to omit the "garbage"
+        self.stack[0..self.sp].to_vec()
     }
 
     fn push(&mut self, val: Value) {
@@ -124,33 +131,31 @@ impl VM {
                     _ => return Err("Invalid operand for JumpIfTrue"),
                 }
             }
+            Instruction::Print => {
+                println!("{:?}", self.pop());
+            }
             Instruction::Call { addr, nargs } => {
                 self.push(Value::Pointer(self.fp));
                 self.push(Value::Pointer(self.ip));
-                // the 2 here accounts for fp and ip on the stack
-                self.fp = self.sp - (nargs as usize) - 2;
+                self.fp = self.sp - (nargs as usize);
                 self.ip = addr;
-                self.stack[self.fp..self.sp].rotate_right(2);
+                self.stack[self.fp - 2..self.sp].rotate_right(2);
             }
-            Instruction::LoadArg { narg } => {
-                // the 2 here accounts for fp and ip on the stack
-                self.push(self.stack[self.fp + (narg as usize) + 2].clone());
+            Instruction::LoadLocal { idx } => {
+                self.push(self.stack[self.fp + (idx as usize)].clone());
             }
             Instruction::Ret => {
                 let ret = self.pop();
-                self.sp = self.fp;
-                self.ip = match self.stack[self.fp + 1] {
+                self.sp = self.fp - 2;
+                self.ip = match self.stack[self.fp - 1] {
                     Value::Pointer(i) => i,
                     _ => return Err("Invalid return address"),
                 };
-                self.fp = match self.stack[self.fp] {
+                self.fp = match self.stack[self.fp - 2] {
                     Value::Pointer(i) => i,
                     _ => return Err("Invalid frame pointer"),
                 };
                 self.push(ret);
-            }
-            Instruction::Print => {
-                println!("{:?}", self.pop());
             }
         }
         Ok(())
@@ -175,6 +180,144 @@ impl VM {
     }
 }
 
+struct Compiler {
+    env_stack: Vec<HashMap<String, usize>>,
+}
+
+impl Compiler {
+    fn new() -> Self {
+        Compiler {
+            env_stack: vec![HashMap::<String, usize>::new()],
+        }
+    }
+
+    fn compile_expr(&mut self, expr: &Expr) -> Result<Vec<Instruction>, String> {
+        match expr {
+            Expr::List(v) => self.compile_sexpr(&v),
+            Expr::Bool(b) => Ok(vec![Instruction::Push {
+                value: Value::Bool(*b),
+            }]),
+            Expr::Integer(i) => Ok(vec![Instruction::Push {
+                value: Value::Int(*i),
+            }]),
+            _ => todo!("{expr}"),
+        }
+    }
+
+    fn compile_sexpr(&mut self, exprs: &[Expr]) -> Result<Vec<Instruction>, String> {
+        let Some((first, rest)) = exprs.split_first() else {
+            return Err("Cannot compile the empty list".to_string());
+        };
+        match first {
+            Expr::Keyword(Keyword::If) => self.compile_if(rest),
+            Expr::Keyword(Keyword::Begin) => self.compile_begin(rest),
+            Expr::Keyword(Keyword::Define) => self.compile_define(rest),
+            Expr::Keyword(Keyword::Lambda) => self.compile_lambda(rest),
+            Expr::Symbol(s) => {
+                let s = s.as_str();
+                match s {
+                    "<" | ">" | "<=" | ">=" => self.compile_cmp(s, rest),
+                    "+" => self.compile_add(rest),
+                    "-" => self.compile_sub(rest),
+                    _ => todo!("list starting with: {first}"),
+                }
+            }
+            _ => todo!("list starting with: {first}"),
+        }
+    }
+
+    fn compile_if(&mut self, args: &[Expr]) -> Result<Vec<Instruction>, String> {
+        if args.len() != 3 {
+            return Err("`if` takes exactly 3 arguments".to_string());
+        }
+        let mut instr = self.compile_expr(&args[0])?;
+        let mut branch_true = self.compile_expr(&args[1])?;
+        let mut branch_false = self.compile_expr(&args[2])?;
+        instr.push(Instruction::JumpIfTrue {
+            offset: branch_false.len() as i16 + 2,
+        });
+        instr.append(&mut branch_false);
+        instr.push(Instruction::Jump {
+            offset: branch_true.len() as i16 + 1,
+        });
+        instr.append(&mut branch_true);
+        Ok(instr)
+    }
+
+    fn compile_begin(&mut self, args: &[Expr]) -> Result<Vec<Instruction>, String> {
+        let mut instr = vec![];
+        for expr in args {
+            instr.append(&mut self.compile_expr(expr)?);
+        }
+        Ok(instr)
+    }
+
+    fn compile_define(&mut self, args: &[Expr]) -> Result<Vec<Instruction>, String> {
+        if args.len() < 2 {
+            return Err("`define` takes at least 2 arguments".to_string());
+        }
+        _ = self.env_stack;
+        todo!("define")
+    }
+
+    fn compile_lambda(&mut self, args: &[Expr]) -> Result<Vec<Instruction>, String> {
+        if args.len() < 1 {
+            return Err("`lambda` takes at least 1 argument".to_string());
+        }
+        todo!("lambda")
+        // TODO:
+        // - add stack of environments (symbols tables) to compiler
+        // - each environment maps each symbol to where the associated datum is
+        // - lambda pushes new environment onto the compiler stack
+        // - compile each expression in the body of the lambda
+        // - concatenate output code?
+    }
+
+    fn compile_cmp(&mut self, cmp: &str, args: &[Expr]) -> Result<Vec<Instruction>, String> {
+        if args.len() != 2 {
+            return Err("`{cmp}` takes exactly 2 arguments".to_string());
+        }
+        let mut instr = self.compile_expr(&args[0])?;
+        let mut instr_op2 = self.compile_expr(&args[1])?;
+        instr.append(&mut instr_op2);
+        match cmp {
+            "<" => instr.push(Instruction::LessThan),
+            _ => todo!("comparison operator: {cmp}"),
+        }
+        Ok(instr)
+    }
+
+    fn compile_add(&mut self, args: &[Expr]) -> Result<Vec<Instruction>, String> {
+        if args.len() != 2 {
+            return Err("`+` takes exactly 2 arguments".to_string());
+        }
+        let mut instr = self.compile_expr(&args[0])?;
+        let mut instr_op2 = self.compile_expr(&args[1])?;
+        instr.append(&mut instr_op2);
+        instr.push(Instruction::Add);
+        Ok(instr)
+    }
+
+    fn compile_sub(&mut self, args: &[Expr]) -> Result<Vec<Instruction>, String> {
+        if args.len() != 2 {
+            return Err("`-` takes exactly 2 arguments".to_string());
+        }
+        let mut instr = self.compile_expr(&args[0])?;
+        let mut instr_op2 = self.compile_expr(&args[1])?;
+        instr.append(&mut instr_op2);
+        instr.push(Instruction::Sub);
+        Ok(instr)
+    }
+}
+
+pub fn compile(exprs: &[Expr]) -> Result<Vec<Instruction>, String> {
+    if exprs.len() != 1 {
+        return Err("Only one expression can be compiled (for now)".to_string());
+    }
+    let mut comp = Compiler::new();
+    comp.compile_expr(&exprs[0])
+}
+
 #[cfg(test)]
 mod tests {
     use super::{Instruction::*, Value::*, VM};
@@ -188,16 +331,16 @@ mod tests {
             Halt,                       // halt
             // fib:
             Push { value: Int(1) }, // push 1
-            LoadArg { narg: 0 },    // put n on the stack
+            LoadLocal { idx: 0 },   // put n on the stack
             LessThan,
             JumpIfTrue { offset: 3 }, // if 1 < n, jump to recursive case
-            LoadArg { narg: 0 },      // put n on the stack
+            LoadLocal { idx: 0 },     // put n on the stack
             Ret,                      // return n
-            LoadArg { narg: 0 },
+            LoadLocal { idx: 0 },
             Push { value: Int(1) },
             Sub,
             Call { addr: 3, nargs: 1 }, // fib(n - 1)
-            LoadArg { narg: 0 },
+            LoadLocal { idx: 0 },
             Push { value: Int(2) },
             Sub,
             Call { addr: 3, nargs: 1 }, // fib(n - 2)
