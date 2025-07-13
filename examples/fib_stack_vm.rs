fc46a69d5a098fb0f5916d62bff56cfe05c444e1
Lorenzo Stella <lorenzostella@gmail.com>
2025-07-13 22:32:50 +0200

    update example

--- examples/fib_stack_vm.rs
+++ examples/fib_stack_vm.rs
@@ -8,18 +8,18 @@ fn main() {
         Halt,                    // halt
         // fib:
         StackAlloc { size: 1 },
-        Push { value: Int(1) }, // push 1
-        LoadArg { idx: 0 },     // put n on the stack
+        Push { value: Int(1) },   // push 1
+        LoadLocal { offset: -3 }, // put n on the stack
         LessThan,
         JumpIfTrue { offset: 3 }, // if 1 < n, jump to recursive case
-        LoadArg { idx: 0 },       // put n on the stack
+        LoadLocal { offset: -3 }, // put n on the stack
         Ret,                      // return n
-        LoadArg { idx: 0 },
+        LoadLocal { offset: -3 },
         Push { value: Int(1) },
         Sub,
         Call { addr: 3 }, // fib(n - 1)
         StoreLocal { offset: 0 },
-        LoadArg { idx: 0 },
+        LoadLocal { offset: -3 },
         Push { value: Int(2) },
         Sub,
         Call { addr: 3 }, // fib(n - 2)
