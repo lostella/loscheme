ab719c21fae2f1a9e01a9ff1ee60e706db325d62
Lorenzo Stella <lorenzostella@gmail.com>
2025-08-15 19:55:49 +0200

    fix fmt

--- tests/test.rs
+++ tests/test.rs
@@ -476,7 +476,10 @@ fn test_language_features() {
             "",
         ),
         ("(five-sum 1 2 3 4)", "50"),
-        ("(include \"./tests/test-1.scm\" \"./tests/test-2.scm\")", ""),
+        (
+            "(include \"./tests/test-1.scm\" \"./tests/test-2.scm\")",
+            "",
+        ),
         ("(f1 42)", "84"),
         ("(f2 42)", "126"),
     ];
