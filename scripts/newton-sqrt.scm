09298979232983a41f28b2ff96d7e24e01686c15
Lorenzo Stella <lorenzostella@gmail.com>
2025-11-16 23:59:26 +0100

    add example script

---
+++ scripts/newton-sqrt.scm
@@ -0,0 +1,19 @@
+(import (scheme write))
+
+(define (sqrt x)
+    (define (square x)
+        (* x x))
+    (define (average x y)
+        (/ (+ x y) 2))
+    (define (good-enough? guess)
+        (< (abs (- (square guess) x)) 0.001))
+    (define (improve guess)
+        (average guess (/ x guess)))
+    (define (sqrt-iter guess)
+        (if (good-enough? guess)
+            guess
+            (sqrt-iter (improve guess))))
+    (sqrt-iter 1.0))
+
+(write (sqrt 2))
+(newline)
