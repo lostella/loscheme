1f49152ff3735df3cbf97ad6c725c6edb835edc6
Lorenzo Stella <lorenzostella@gmail.com>
2026-04-07 22:45:04 +0200

    fix example

--- examples/fib_stack_vm_handcrafted.rs
+++ examples/fib_stack_vm_handcrafted.rs
@@ -10,18 +10,18 @@ fn main() {
             // fib(n):  n is at LoadLocal { offset: -3 }
             StackAlloc { size: 1 },   // local[0] for fib(n-1) result
             PushOne,                  // push 1
-            LoadLocal { offset: -3 }, // push n
+            LoadLocal { offset: 3 },  // push n
             LessThan,                 // 1 < n ?
             JumpIfTrue { offset: 2 }, // if true, jump to recursive case
-            LoadLocal { offset: -3 }, // push n
+            LoadLocal { offset: 3 },  // push n
             Ret,                      // return n (base case)
             // recursive case:
-            LoadLocal { offset: -3 }, // push n
+            LoadLocal { offset: 3 },  // push n
             PushOne,                  // push 1
             Sub,                      // n - 1
             Call { addr: 3 },         // fib(n-1)
             StoreLocal { offset: 0 }, // local[0] = fib(n-1)
-            LoadLocal { offset: -3 }, // push n
+            LoadLocal { offset: 3 },  // push n
             LoadConst { offset: 1 },  // push 2
             Sub,                      // n - 2
             Call { addr: 3 },         // fib(n-2)
